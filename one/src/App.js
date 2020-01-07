1cadd53cfd69fd8df6d34da6affccf6871b83233
Ritveak Dugar <ritveakdugar2014244@gmail.com>
2020-01-07 16:05:20 +0530

    included children

--- one/src/App.js
+++ one/src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>This is React App</h1>
         <p>This part is main app section</p>
-        <Person name ="Max" age ="12"></Person>
-        <Person name ="Mark" age ="23"></Person>
-        <Person name ="Marshall" age ="32"></Person>
+        <Person name ="Max" age ="12">"Yo I am an engineer"</Person>
+        <Person name ="Mark" age ="23">"I love Badminton"</Person>
+        <Person name ="Marshall" age ="32">"I am done with hobbies"</Person>
       </div>
     );
   }
--- one/src/Person/Person.js
+++ one/src/Person/Person.js
@@ -3,7 +3,10 @@ import React from 'react';
 const person =(props) =>{
     return (
     // <p> This is a person and that person is {Math.floor(Math.random()*30)}  </p>
+<div>
 <p>This is {props.name}, who is {props.age} years old.</p>
+    <p>{props.children}</p>
+</div>
     )
 };
 
