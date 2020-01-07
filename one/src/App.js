9a59bc999484db3ddf2e79bff28639d308d6d99b
Ritveak Dugar <ritveakdugar2014244@gmail.com>
2020-01-07 16:41:49 +0530

    update

--- one/src/App.js
+++ one/src/App.js
@@ -3,14 +3,34 @@ import Person from './Person/Person.js' ;
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    persons:[
+      { name:'Max', age:12 },
+      { name :'Mark', age:23 },
+      { name :'Marshall', age:32}
+    ]
+  }
+
+  switchName=() =>{
+    this.setState({
+      persons:[
+        { name:'Max 2.0', age:12 },
+      { name :'Mark 2.0', age:23 },
+      { name :'Marshall 2.0', age:32}
+      ]
+    })
+    this.state.persons[0].name = "Max 2.0";
+  }
   render() {
     return (
       <div className="App">
         <h1>This is React App</h1>
         <p>This part is main app section</p>
-        <Person name ="Max" age ="12">"Yo I am an engineer"</Person>
-        <Person name ="Mark" age ="23">"I love Badminton"</Person>
-        <Person name ="Marshall" age ="32">"I am done with hobbies"</Person>
+        <button onClick={this.switchName}>Switch Name</button>
+        <Person name ={this.state.persons[0].name} age ={this.state.persons[0].age}>"Yo I am an engineer"</Person>
+        <Person name ={this.state.persons[1].name} age ={this.state.persons[1].age}>"I love Badminton"</Person>
+        <Person name ={this.state.persons[2].name} age ={this.state.persons[2].age}>"I am done with hobbies"</Person>
       </div>
     );
   }
